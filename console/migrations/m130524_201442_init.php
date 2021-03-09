0c9279a7796bd48b04b3c0719e9f5b00d0fb26b1
Maxim <51275206+Xrona@users.noreply.github.com>
2021-03-09 09:32:25 +0500

    Update m130524_201442_init.php

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -24,6 +24,18 @@ class m130524_201442_init extends Migration
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+        
+        $this->insert('{{%user}}', [
+            'id' => 1,
+            'username' => 'webmaster',
+            'auth_key' => Yii::$app->getSecurity()->generateRandomString(),
+            'password_hash' => Yii::$app->getSecurity()->generatePasswordHash('webmaster'),
+            'password_reset_token' => Yii::$app->getSecurity()->generateRandomString(40),
+            'email' => 'webmaster@example.com',
+            'status' => 10,
+            'created_at' => time(),
+            'updated_at' => time()
+        ]);
     }
 
     public function down()
